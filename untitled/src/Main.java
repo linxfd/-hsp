efc12442647cdf640645f726b95b9be8d5816342
linxfd <99418231+linxfd@users.noreply.github.com>
2023-11-08 19:14:51 +0800

    Delete untitled directory

--- untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public class Main  {
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress address = InetAddress.getByName("baidu.com");
-        System.out.println(address);
-    }
-}
